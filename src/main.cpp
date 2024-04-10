910ffaf01b8cffdd4a11b4b28e19f588c345557d
Shashank Agarwal <shashank.agarwal@sindia.co.in>
2024-04-10 10:05:58 +0530

    changed license file

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,12 @@
-/*
-  shashankmarch27@gmail.com
-
-                     GNU GENERAL PUBLIC LICENSE
-                        Version 3, 29 June 2007
-
-  Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
-  Everyone is permitted to copy and distribute verbatim copies
-  of this license document, but changing it is not allowed.
-
-  The GNU General Public License is a free, copyleft license for
-  software and other kinds of works.
-
-  THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY
-  APPLICABLE LAW.  EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT
-  HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM "AS IS" WITHOUT WARRANTY
-  OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO,
-  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-  PURPOSE.  THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE PROGRAM
-  IS WITH YOU.  SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF
-  ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
-*/
+/**
+ * @file main.cpp
+ * @brief Main application file.
+ * @author Shashank Agarwal
+ * @author Gagan Space
+ * @date 10 April , 2024
+ *
+ * @license GNU General Public License Version 3
+ */
 
 #include <Adafruit_BNO08x.h>
 #include <Adafruit_DPS310.h>
@@ -263,6 +250,9 @@ void loop() {
     telemetry.tiltY = ypr.roll;
   }
 #endif
+
+  Serial.write((uint8_t *)&telemetry, sizeof(telemetry));
+  delay(1000);
 }
 
 // put function definitions here:
